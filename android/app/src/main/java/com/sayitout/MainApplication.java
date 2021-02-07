7e0a5ff7c6312510523e40774adc3c2deae1b71c
Joaquín Traverso <jgt@cloudingsystems.co.uk>
2021-02-07 23:05:53 +0100

    swapping libraries

--- android/app/src/main/java/com/sayitout/MainApplication.java
+++ android/app/src/main/java/com/sayitout/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import dog.craftz.sqlite_2.RNSqlite2Package;
 import net.no_mad.tts.TextToSpeechPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
